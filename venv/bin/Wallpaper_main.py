c75c580443e0db54243941364fedb035cef58c35
yatharthrawat <rawat.yatharth@gmail.com>
2018-03-09 23:51:46 -0500

    2nd Commit
      Adds time functionality to the program.Wallpaper changed exactly at 12:00 PM.

--- venv/bin/Wallpaper_main.py
+++ venv/bin/Wallpaper_main.py
@@ -1,42 +1,56 @@
-#This Python Program pulls the Bing Daily Wallpaper and makes it the wallpaper of your mac.
-
-#Created By Yatharth Rawat
-
+# This Python Program pulls the Bing Daily Wallpaper and makes it the wallpaper of your Mac.
+# Created By Yatharth Rawat
 
 from bs4 import BeautifulSoup
 import urllib.request
 from appscript import app, mactypes
 from pathlib import Path
 import os
+import time
 
-home = str(Path.home())
+# Creates a string with the current system time (24 Hour Format)
+now = time.strftime("%H:%M:%S")
 
-newpath=home+'/Documents/Bing_photos/'
-if not os.path.exists(newpath):
-    os.makedirs(newpath)
+# Time set for the Wallpaper to be downloaded and set
+set_time = "12:00:00"
 
-base_url="http://bing.com"
+while now != set_time:
+    now = time.strftime("%H:%M:%S")
+    time.sleep(1)
 
-bing=urllib.request.urlopen("http://bing.com")
-soup = BeautifulSoup(bing,"html.parser")
-script = soup.find_all('script')
+if now == set_time:
 
-script_img=str(script)
+    # Saves the value of the System's HOME variable
+    home = str(Path.home())
+    # Creates a new folder named Bing_photos in the Documents directory if it has not been created
+    newpath = home + '/Documents/Bing_photos/'
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
 
-x=script_img.find("g_img={url:")
-y=script_img.find(".jpg",x)
-img_url=script_img[x+len("g_img={url:")+2:y+len(".jpg")]
-r=img_url.find(".")
-q=len(img_url)-img_url[::-1].find("/")
-img_name=img_url[q:r]
+    base_url = "http://bing.com"
 
-print("URL Located Preparing to Download...")
+    # Creating BeautifulSoup object to perform web scraping
+    bing = urllib.request.urlopen("http://bing.com")
+    soup = BeautifulSoup(bing, "html.parser")
+    script = soup.find_all('script')
 
-f=open(newpath+img_name+".jpg",'wb')
-f.write(urllib.request.urlopen(base_url+img_url).read())
-f.close()
-print("Download Complete Preparing to Change Wallpaper...")
-app('Finder').desktop_picture.set(mactypes.File(newpath+img_name+'.jpg'))
-print("Wallpaper Changed Successfully!\nWallpaper Name:"+img_name)
+    script_img = str(script)
 
+    # Finding the url with the background image
+    x = script_img.find("g_img={url:")
+    y = script_img.find(".jpg", x)
+    img_url = script_img[x + len("g_img={url:") + 2:y + len(".jpg")]
+    r = img_url.find(".")
+    q = len(img_url) - img_url[::-1].find("/")
+    img_name = img_url[q:r]
 
+    print("URL Located Preparing to Download...")
+
+    # Downloading the image in the Bing_photos folder
+    f = open(newpath + img_name + ".jpg", 'wb')
+    f.write(urllib.request.urlopen(base_url + img_url).read())
+    f.close()
+    # setting picture as the wallpaper using appscript
+    print("Download Complete Preparing to Change Wallpaper...")
+    app('Finder').desktop_picture.set(mactypes.File(newpath + img_name + '.jpg'))
+    print("Wallpaper Changed Successfully!\nWallpaper Name:" + img_name)
